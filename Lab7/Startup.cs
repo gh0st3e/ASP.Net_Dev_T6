e9ceefe11b5b2e16a2599266413f68641b10df9c
gh0st3e <bigdenbstu@yandex.by>
2023-08-03 19:25:49 +0300

    Auth

---
+++ Lab7/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab7.Startup))]
+namespace Lab7
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
